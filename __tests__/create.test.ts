a99810fd156a910cd67fe6f2bdae8fa06d59f2b6
Ichinose Shogo <shogo82148@gmail.com>
2021-10-16 16:23:46 +0900

    npm run format

--- __tests__/create.test.ts
+++ __tests__/create.test.ts
@@ -1,38 +1,38 @@
-import * as release from '../src/create-release'
+import * as release from "../src/create-release";
 
-test('Create a new release', async () => {
+test("Create a new release", async () => {
   const createRelease = jest.fn().mockReturnValue({
     id: 123,
-    html_url: 'http://example.com/html',
-    upload_url: 'http://example.com/upload'
-  })
+    html_url: "http://example.com/html",
+    upload_url: "http://example.com/upload",
+  });
 
   const resp = await release.create({
-    github_token: 'very-secret',
-    tag_name: 'v1.0.0',
-    release_name: '',
-    body: '',
-    body_path: '',
+    github_token: "very-secret",
+    tag_name: "v1.0.0",
+    release_name: "",
+    body: "",
+    body_path: "",
     draft: false,
     prerelease: false,
-    commitish: '',
-    owner: 'shogo82148',
-    repo: 'github-action-test',
-    discussion_category_name: '',
-    createRelease: createRelease
-  })
-  expect(createRelease).toBeCalledTimes(1)
+    commitish: "",
+    owner: "shogo82148",
+    repo: "github-action-test",
+    discussion_category_name: "",
+    createRelease: createRelease,
+  });
+  expect(createRelease).toBeCalledTimes(1);
   expect(createRelease).toHaveBeenCalledWith({
-    github_token: 'very-secret',
-    tag_name: 'v1.0.0',
+    github_token: "very-secret",
+    tag_name: "v1.0.0",
     name: undefined,
     body: undefined,
     draft: false,
     prerelease: false,
-    owner: 'shogo82148',
-    repo: 'github-action-test'
-  })
-  expect(resp.id).toBe('123')
-  expect(resp.html_url).toBe('http://example.com/html')
-  expect(resp.upload_url).toBe('http://example.com/upload')
-})
+    owner: "shogo82148",
+    repo: "github-action-test",
+  });
+  expect(resp.id).toBe("123");
+  expect(resp.html_url).toBe("http://example.com/html");
+  expect(resp.upload_url).toBe("http://example.com/upload");
+});
