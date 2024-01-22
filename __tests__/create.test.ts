e6ed2b80dd61821818b9fd2b88a20a5c5b8c5293
ICHINOSE Shogo <shogo82148@gmail.com>
2024-01-23 00:26:58 +0900

    add test for notes-start-tag

--- __tests__/create.test.ts
+++ __tests__/create.test.ts
@@ -305,3 +305,73 @@ test("overwrite not-existing release", async () => {
   expect(resp.html_url).toBe("http://example.com/html");
   expect(resp.upload_url).toBe("http://example.com/upload");
 });
+
+test("create a new release with generated release not", async () => {
+  const generateReleaseNotes = jest
+    .spyOn(github.Client.prototype, "generateReleaseNotes")
+    .mockImplementation(
+      async (
+        params: github.GenerateReleaseNotesParams,
+      ): Promise<github.Result<github.GenerateReleaseNotesResponse, github.GitHubError>> => {
+        expect(params).toEqual({
+          owner: "shogo82148",
+          repo: "github-action-test",
+          tag_name: "v1.0.0",
+          target_commitish: undefined,
+          previous_tag_name: "v0.1.0",
+        });
+        return new github.Success({
+          body: "generated release notes",
+          name: "Release v1.0.0",
+        });
+      },
+    );
+  const createRelease = jest
+    .spyOn(github.Client.prototype, "createRelease")
+    .mockImplementation(
+      async (
+        params: github.CreateReleaseParams,
+      ): Promise<github.Result<github.CreateReleaseResponse, github.GitHubError>> => {
+        expect(params).toEqual({
+          owner: "shogo82148",
+          repo: "github-action-test",
+          tag_name: "v1.0.0",
+          name: "Release v1.0.0",
+          body: "generated release notes",
+          target_commitish: undefined,
+          draft: false,
+          prerelease: false,
+          discussion_category_name: undefined,
+          generate_release_notes: false,
+        });
+        return new github.Success({
+          id: 123,
+          html_url: "http://example.com/html",
+          upload_url: "http://example.com/upload",
+        });
+      },
+    );
+
+  const client = new github.Client("", "http://localhost:1234");
+  const resp = await release.create({
+    client,
+    tag_name: "v1.0.0",
+    release_name: "",
+    body: "",
+    body_path: "",
+    draft: false,
+    prerelease: false,
+    commitish: "",
+    owner: "shogo82148",
+    repo: "github-action-test",
+    discussion_category_name: "",
+    generate_release_notes: true,
+    notes_start_tag: "v0.1.0",
+    overwrite: false,
+  });
+  expect(generateReleaseNotes).toHaveBeenCalled();
+  expect(createRelease).toHaveBeenCalled();
+  expect(resp.id).toBe("123");
+  expect(resp.html_url).toBe("http://example.com/html");
+  expect(resp.upload_url).toBe("http://example.com/upload");
+});
